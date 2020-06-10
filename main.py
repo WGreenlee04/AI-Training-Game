c9f0c35d177ef8a01fed3d25b88e01b4ea007c2b
WGreenlee04 <greenlee04@gmail.com>
2020-06-10 16:27:48 -0400

    Base Code
    Module has been updated, and libraries are installed. Code functions to generate a black window with a sword icon called "ShooterGame" despite this being a fighting game.

---
+++ main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import pygame
+
+
+# main function
+def main():
+	# initialize pygame
+	pygame.init()
+	# load logo
+	logo_path = Path("resources/sprites/logo320x320.png")
+	logo = pygame.image.load(str(logo_path))  # logo has the type 'surface' which is like an image
+	# congfigure display window
+	pygame.display.set_icon(logo)
+	pygame.display.set_caption("ShooterGame")
+
+	# use current flags (set above) to render window
+	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+	running = True
+
+	while running:
+		# for each event in the event queue
+		for event in pygame.event.get():
+			# event handler
+			if event.type == pygame.QUIT:
+				running = False
+
+	pygame.quit()
+
+
+# if this is the main process
+if __name__ == '__main__':
+	main()
