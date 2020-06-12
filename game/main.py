c14caf845489e8811f6fa326e11a8899521e3e57
WGreenlee04 <greenlee04@gmail.com>
2020-06-12 13:22:16 -0400

    Update main.py
    Now draws a black fullscreen window using pyglet (openGL) and displays white text which says "Hello, world"
    
    Credit to Pqvqn for helping decide on the library to be used for the game.

--- game/main.py
+++ game/main.py
@@ -1,29 +1,20 @@
-from pathlib import Path
-
-import pygame
+import pyglet
+from pyglet.gl import *
 
 
 # main function
 def main():
-	# initialize pygame
-	pygame.init()
-	# load logo
-	# logo has the type 'surface' which is like an Image in any other language
-	logo = pygame.image.load(str(Path("resources/sprites/logo320x320.png")))
-	# congfigure display window
-	pygame.display.set_icon(logo)
-	pygame.display.set_caption("ShooterGame")
+	window = pyglet.window.Window(1920, 1080)
+	label = pyglet.text.Label('Hello, world', font_name='Times New Roman', font_size=36,
+	                          x=window.width // 2, y=window.height // 2,
+	                          anchor_x='center', anchor_y='center')
+	window.set_location(0, 0)
+	window.set_fullscreen(True)
+	window.set_caption("ShooterGame")
 
-	# use current flags (set above) to render window
-	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+	@window.event
+	def on_draw():
+		window.clear()
+		label.draw()
 
-	running = True
-
-	while running:
-		# for each event in the event queue
-		for event in pygame.event.get():
-			# event handler
-			if event.type == pygame.QUIT:
-				running = False
-
-	pygame.quit()
+	pyglet.app.run()
